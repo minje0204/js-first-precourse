f025a9ad5b69a5ec8345d61eaff1af7d12d05f2f
minje cho <minje@ajou.ac.kr>
2021-11-10 21:58:44 +0900

    사용자 인풋 저장

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 class App {
   constructor($target) {
     this.$target = $target;
+    this.$input = document.querySelector('#user-input');
+    this.$submitButton = document.querySelector('#submit');
     this.$resultTag = document.querySelector('#result');
+
+    this.initialize();
     this.render();
   }
 
+  initialize() {
+    this.$target.addEventListener('click', (e) => this.handleClick(e));
+  }
+
+  handleClick(e) {
+    if (e.target.id === 'submit') {
+      const userInput = this.$input.value;
+    }
+  }
+
   render() {
+    this.$resultTag.innerHTML = '';
     const result = document.createElement('div');
     result.innerHTML = '레이아웃 테스트';
     this.$resultTag.appendChild(result);
